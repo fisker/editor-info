a706e9fc17bfaa624f705b1d72487cc1e0457e04
fisker Cheung <lionkay@gmail.com>
2021-05-24 10:13:46 +0800

    Update snapshot

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const editors = [
   {
-    name: 'Visual Studio Code',
-    // https://github.com/microsoft/vscode/blob/949f60498aecf6f78f63aa587999589a8f65c3ac/src/vs/code/electron-main/main.ts#L194
-    constant: 'VSCODE',
-    environmentKey: 'VSCODE_PORTABLE',
-  },
-  {
     name: 'Atom',
     // https://flight-manual.atom.io/using-atom/sections/basic-customization/#custom-home-location-with-an-environment-variable
     constant: 'ATOM',
     environmentKey: 'ATOM_HOME',
+  },
+  {
+    name: 'Visual Studio Code',
+    // https://github.com/microsoft/vscode/blob/949f60498aecf6f78f63aa587999589a8f65c3ac/src/vs/code/electron-main/main.ts#L194
+    constant: 'VSCODE',
+    environmentKey: 'VSCODE_PORTABLE',
   },
 ]
 
