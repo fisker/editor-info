6d7cd29f6feba33ddcd7fde9cd718ddaaa5c8b7b
fisker Cheung <lionkay@gmail.com>
2021-05-24 10:29:40 +0800

    Fix when multiple environment key seen

--- test/main.js
+++ test/main.js
@@ -6,7 +6,15 @@ test('main', (t) => {
 })
 
 test('vscode', (t) => {
-  process.env.VSCODE_PORTABLE = ''
+  process.env.VSCODE_PORTABLE = '/code'
   t.snapshot(importFresh('../index.js'))
   delete process.env.VSCODE_PORTABLE
+})
+
+test('both vscode and atom environment are set', (t) => {
+  process.env.VSCODE_PORTABLE = '/code'
+  process.env.ATOM_HOME = '/atom'
+  t.snapshot(importFresh('../index.js'))
+  delete process.env.VSCODE_PORTABLE
+  delete process.env.ATOM_HOME
 })
